cd9c635d79bce6d1ad5ec757a2c5d0af0daafd5f
masangwoo <masw23@naver.com>
2022-04-08 15:08:03 +0900

    msa practices

--- services/service-storage/src/main/java/me/kickscar/msa/service/storage/ServiceStorage.java
+++ services/service-storage/src/main/java/me/kickscar/msa/service/storage/ServiceStorage.java
@@ -16,7 +16,8 @@ public class ServiceStorage {
 	}
 
 	@Bean
-	ApplicationRunner HttpdStarter() {
+	ApplicationRunner httpdStarter() {
+		
 		return new ApplicationRunner() {
 			@Autowired
 			private SimpleHttpd httpd;
